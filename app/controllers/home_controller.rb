4306fd82cc290fd93903ec92dd031e4014fc0ae1
Fernando <fernandobrazror@gmail.com>
2015-02-23 16:59:00 +0000

    Add the header with user setings and the menu with links. also remove extra info from user pages like last ip logged in..

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
+    @last_incomes = current_user.incomes.last(5)
+    @current_month_outcomes = current_user.outcomes.where("paymentDate <= ?", Date.today.end_of_month).where("paymentDate >= ?", Date.today.beginning_of_month)
+    @last_outcomes = current_user.outcomes.last(5)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,20 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :incomes
   has_many :outcomes
+
+  def total_money
+    #self.incomes.first.value
+    @out_total = 0
+    @inc_total = 0
+    self.incomes.each do |inc|
+
+      @inc_total = @inc_total + inc.value
+    end
+    self.outcomes.each do |out|
+
+      @out_total = @out_total + out.value
+    end
+    result = @inc_total - @out_total
+    return result
+  end
 end
